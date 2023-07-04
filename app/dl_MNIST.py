4b517689a0cd0ab960ffc690f8702e68b83773f1
Tinoeike <kajiwara.ryutaro.ku9@is.naist.jp>
2023-07-04 23:15:03 +0900

    実行場所によらずにデータが保存されるようにしました.

--- app/dl_MNIST.py
+++ app/dl_MNIST.py
@@ -1,3 +1,4 @@
+import os
 import torch
 import torchvision
 import torchvision.transforms as T
@@ -6,10 +7,12 @@ from torch.utils.data import DataLoader
 # MNISTの取得
 transform = T.Compose([T.ToTensor()])
 
+path = os.path.dirname(os.path.abspath(__file__))
+
 # 学習データ
-train_data = torchvision.datasets.MNIST(root='../dataset/MNIST', train=True,download=True,transform=transform)
+train_data = torchvision.datasets.MNIST(root='{}/../dataset/'.format(path), train=True,download=True,transform=transform)
 train_loader = DataLoader(train_data,batch_size = 64)
 
 # テストデータ
-test_data = torchvision.datasets.MNIST(root='../dataset/MNIST', train=False,download=True,transform=transform)
+test_data = torchvision.datasets.MNIST(root='{}/../dataset/'.format(path), train=False,download=True,transform=transform)
 test_loader = DataLoader(test_data,batch_size = 64)
