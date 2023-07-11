044170d16d6c5951b80197c872c9edc1888a5e7b
Tinoeike <kajiwara.ryutaro.ku9@is.naist.jp>
2023-07-11 17:23:17 +0900

    Encoderクラスの実装をしました

---
+++ app/autoencoder.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+
+class Encoder(nn.Module):
+    def __init__(self, input_size, z_dimention):
+        super().__init__()
+        self.layer1 = nn.Linear(input_size, 256)
+        self.layer2 = nn.Linear(256, 64)
+        self.layer3 = nn.Linear(64, z_dimention)
+
+    def forward(self,x):
+        x = self.layer1(x)
+        x = torch.relu(x)
+        x = self.layer2(x)
+        x = torch.relu(x)
+        x = self.layer3(x)
+        x = torch.relu(x)
+        return x
+
