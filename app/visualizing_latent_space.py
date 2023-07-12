1d9818500d72767890fec3b3e8bf723059a8c416
Tinoeike <kajiwara.ryutaro.ku9@is.naist.jp>
2023-07-12 18:57:06 +0900

    潜在空間の可視化を実装しました

---
+++ app/visualizing_latent_space.py
@@ -0,0 +1,31 @@
+import args
+import dl_MNIST
+import autoencoder
+
+import torch
+import numpy as np
+from torch.autograd import Variable
+
+# 学習済のモデルを読み込む
+autoencoder.model.load_state_dict(torch.load('./autoencoder.pth', map_location=lambda storage, loc: storage))
+
+test_batchsize = 10000
+test_loader = dl_MNIST.DataLoader(dl_MNIST.test_data, batch_size=test_batchsize, shuffle=False)
+
+images, labels = next(iter(test_loader))
+images = images.view(test_batchsize, -1).to(autoencoder.device)
+
+# 784次元ベクトルを2次元ベクトルにencode
+encoded = autoencoder.model.encoder(Variable(images, volatile=True))
+z = encoded.cpu().data.numpy()
+
+
+import pylab
+import matplotlib.pyplot as plt
+
+# Encoderによって生成された潜在空間を可視化
+plt.figure(figsize=(10, 10))
+plt.scatter(z[:, 0], z[:, 1], marker='.', c=labels.numpy(), cmap=pylab.cm.jet)
+plt.colorbar()
+plt.grid()
+plt.show()
